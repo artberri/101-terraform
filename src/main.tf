1e0be3cd70581c48899c5430241277aad817f793
Alberto Varela <alberto@berriart.com>
2017-09-25 00:45:06 +0200

    More resources

--- src/main.tf
+++ src/main.tf
@@ -1,9 +1,65 @@
-resource "aws_instance" "example" {
-    ami           = "ami-e1f2e185" // Ubuntu 16.04 LTS hvm:ebs-ssd
-    instance_type = "t2.micro"
+resource "aws_key_pair" "acme" {
+    key_name   = "acme"
+    public_key = "${file("../admin_ssh_key.pub")}"
+}
 
-    tags {
-        Name        = "Example Machine"
-        Environment = "Production"
-    }
+resource "aws_s3_bucket" "acme" {
+    bucket = "acme-terraform-101-images"
+    acl    = "public-read"
+}
+
+# Create a new load balancer
+resource "aws_elb" "acme" {
+  name               = "acme-elb"
+  availability_zones = ["eu-west-2a", "eu-west-2b"]
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+}
+
+resource "aws_instance" "acme1" {
+    ami                         = "ami-e1f2e185" // Ubuntu 16.04 LTS hvm:ebs-ssd
+    instance_type               = "t2.micro"
+    associate_public_ip_address = true
+    key_name                    = "acme"
+}
+
+resource "aws_elb_attachment" "acme1" {
+  elb      = "${aws_elb.acme.id}"
+  instance = "${aws_instance.acme1.id}"
+}
+
+resource "aws_instance" "acme2" {
+    ami                         = "ami-e1f2e185" // Ubuntu 16.04 LTS hvm:ebs-ssd
+    instance_type               = "t2.micro"
+    associate_public_ip_address = true
+    key_name                    = "acme"
+}
+
+resource "aws_elb_attachment" "acme2" {
+  elb      = "${aws_elb.acme.id}"
+  instance = "${aws_instance.acme2.id}"
+}
+
+resource "aws_db_instance" "acme" {
+  allocated_storage    = 5
+  storage_type         = "standard"
+  engine               = "mysql"
+  engine_version       = "5.7.17"
+  instance_class       = "db.t2.micro"
+  name                 = "acme"
+  username             = "acme"
+  password             = "12345678"
 }
--- src/output.tf
+++ src/output.tf
@@ -1,3 +1,15 @@
-output "example_ip" {
-    value = "${aws_instance.example.public_ip}"
+output "acme_dns" {
+    value = "${aws_elb.acme.dns_name}"
+}
+
+output "acme1_ip" {
+    value = "${aws_instance.acme1.public_ip}"
+}
+
+output "acme2_ip" {
+    value = "${aws_instance.acme2.public_ip}"
+}
+
+output "acme_db_address" {
+    value = "${aws_db_instance.acme.address}"
 }
