a3861513d2556a6b8e867c7fe3e6cef5fb227492
Alberto Varela <alberto@berriart.com>
2018-04-17 18:42:33 +0200

    Removing explicit backend

--- src/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        storage_account_name = "101terraformstates"
-        container_name       = "plaintfstate"
-        key                  = "prod.terraform.tfstate"
-        resource_group_name  = "101-terraform-states"
-    }
-}
