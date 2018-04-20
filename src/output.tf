e18784124456623137f5714d9179df60b10f3900
Alberto Varela <alberto@berriart.com>
2018-04-20 21:58:52 +0200

    Adding one sample client

--- src/output.tf
+++ src/output.tf
@@ -1,7 +1,3 @@
-// output "project1_load_balancer_ip" {
-//     value = "${module.project1_instances.load_balancer_ip}"
-// }
-
-// output "project2_load_balancer_ip" {
-//     value = "${module.project2_instances.load_balancer_ip}"
-// }
+output "client_acme_load_balancer_ip" {
+    value = "${module.client_acme.load_balancer_ip}"
+}
