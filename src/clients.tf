b954514062d15a4282a4cb0b86bf05be1e052c9c
Alberto Varela <alberto@berriart.com>
2018-04-20 22:37:20 +0200

    Removing instances

--- src/clients.tf
+++ src/clients.tf
@@ -4,7 +4,7 @@ module "client_acme" {
     resource_group    = "${azurerm_resource_group.terraform_sample.name}"
     location          = "${azurerm_resource_group.terraform_sample.location}"
     subnet_id         = "${azurerm_subnet.my_subnet_frontend.id}"
-    instance_count    = 2
+    instance_count    = 0
     instance_size     = "Standard_A0"
     instance_user     = "${var.arm_vm_admin_user}"
     instance_password = "${var.arm_vm_admin_password}"
