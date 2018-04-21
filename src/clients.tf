e127209e1097256f456b3fba1a6ccf8794b3967a
Alberto Varela <alberto@berriart.com>
2018-04-21 16:24:30 +0200

    En plena demo

--- src/clients.tf
+++ src/clients.tf
@@ -4,19 +4,19 @@ module "client_acme" {
     resource_group    = "${azurerm_resource_group.terraform_sample.name}"
     location          = "${azurerm_resource_group.terraform_sample.location}"
     subnet_id         = "${azurerm_subnet.my_subnet_frontend.id}"
-    instance_count    = 0
+    instance_count    = 2
     instance_size     = "Standard_A0"
     instance_user     = "${var.arm_vm_admin_user}"
     instance_password = "${var.arm_vm_admin_password}"
 }
 
-module "client_other" {
+module "client_ms" {
     source            = "./modules/azurerm/ubuntu-vms-with-lb"
-    prefix            = "other"
+    prefix            = "ms"
     resource_group    = "${azurerm_resource_group.terraform_sample.name}"
     location          = "${azurerm_resource_group.terraform_sample.location}"
     subnet_id         = "${azurerm_subnet.my_subnet_frontend.id}"
-    instance_count    = 2
+    instance_count    = 1
     instance_size     = "Standard_A0"
     instance_user     = "${var.arm_vm_admin_user}"
     instance_password = "${var.arm_vm_admin_password}"
